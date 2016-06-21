1dabb6a6ecfb784057b16ab6a746b32b012031b6
heschlie@gmail.com <Big!1Dog$4>
2016-06-20 20:46:57 -0700

    Functional tests now check for todo items

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import unittest
 
 class NewVisitor(unittest.TestCase):
@@ -16,6 +17,21 @@ class NewVisitor(unittest.TestCase):
         self.browser.get('http://localhost:8000')
 
         self.assertIn('To-Do', self.browser.title)
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
+
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(inputbox.get_attribute('placeholder'),
+                         'Enter a to-do item')
+
+        inputbox.send_keys('Buy peackock feathers')
+
+        inputbox.send_keys(Keys.ENTER)
+
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(any(row.text == '1: Buy peackock feathers' for row in rows))
+
         self.fail('Finish the test!')
 
 if __name__ == '__main__':
