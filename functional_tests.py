e5ca01b0db792d053ae68dc0375bea17b71ea649
heschlie@gmail.com <Big!1Dog$4>
2016-06-21 09:00:04 -0700

    Actually added template, started adding code to make functional tests pass

--- functional_tests.py
+++ functional_tests.py
@@ -30,7 +30,8 @@ class NewVisitor(unittest.TestCase):
 
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
-        self.assertTrue(any(row.text == '1: Buy peackock feathers' for row in rows))
+        self.assertTrue(any(row.text == '1: Buy peackock feathers' for row in rows),
+                        'New to-do item did not appear in table')
 
         self.fail('Finish the test!')
 
