926a306c01935121262d38b171f716d91bf14313
heschlie@gmail.com <Big!1Dog$4>
2016-08-19 10:43:06 -0700

    some changes

--- lists/models.py
+++ lists/models.py
@@ -6,5 +6,5 @@ class List(models.Model):
 
 
 class Item(models.Model):
-    text = models.TextField(default='', blank=True)
+    text = models.TextField(default='')
     list = models.ForeignKey(List, default=None)
